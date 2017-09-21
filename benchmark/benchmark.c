8510e98ce13caa5d0d82ac9231916a9c92334b3a
rishabhprathore <rrathor@ncsu.edu>
2017-09-21 19:46:06 -0400

    test

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -67,7 +67,9 @@ int main(int argc, char *argv[])
         {
             sprintf(mapped_data,"%s%d",mapped_data,a);
         }
+
         fprintf(fp,"S\t%d\t%ld\t%d\t%lu\t%s\n",getpid(),current_time.tv_sec * 1000000 + current_time.tv_usec,i,strlen(mapped_data),mapped_data);
+        printf("%d: %d: %d",getpid(), i, size);
         npheap_unlock(devfd,i);
     }
     /*
