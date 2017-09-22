5faceeca5dfe06d33cd7a34a00a2bf90a7ade6c4
rishabhprathore <rrathor@ncsu.edu>
2017-09-21 21:42:02 -0400

    test

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -74,12 +74,11 @@ int main(int argc, char *argv[])
     }
     
     // try delete something
-    i = rand()%256;
+    i = rand()%number_of_objects;
     npheap_lock(devfd,i);
     npheap_delete(devfd,i);
     fprintf(fp,"D\t%d\t%ld\t%d\t%lu\t%s\n",pid,current_time.tv_sec * 1000000 + current_time.tv_usec,i,strlen(mapped_data),mapped_data);
     npheap_unlock(devfd,i);
-    
     close(devfd);
     if(pid != 0)
         wait(NULL);
